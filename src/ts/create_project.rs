f350740e80e1de2a4942b9d1b12618ce10926f61
root <root@GFT-kvLUpwGqhUS.gft.com>
2023-02-27 01:40:37 -0300

    0.1.2

--- src/ts/create_project.rs
+++ src/ts/create_project.rs
@@ -11,10 +11,197 @@ pub fn create_project(project_name: &str, language: &str) {
     create_jest_config_file(project_name).unwrap();
     create_main_file_test(project_name).unwrap();
     create_prettier_file(project_name).unwrap();
+    create_docker_file(project_name).unwrap();
+    create_docker_compose_file(project_name).unwrap();
+    create_docker_compose_override_file(project_name).unwrap();
+    create_dot_dockercontainer_folder(project_name).unwrap();
+    create_devcontainer_json_file(project_name).unwrap();
+    create_docker_compose_yml(project_name).unwrap();
+    create_dot_docker_folder(project_name).unwrap();
+    create_start_sh_file(project_name).unwrap();
+    create_gitkeep_file(project_name).unwrap();
+    create_p10k_file(project_name).unwrap();
     install_dependencies(project_name).unwrap();
-    start_git(project_name).unwrap();
     format_files(project_name).unwrap();
+    // start_git(project_name).unwrap();
     open_vscode(project_name).unwrap();
+}
+
+fn create_gitkeep_file(project_name: &str) -> std::io::Result<()> {
+    let mut gitkeep = fs::File::create(format!("{}/.docker/zsh/history/.gitkeep", project_name))?;
+    gitkeep.write_all("".as_bytes()).unwrap();
+    Ok(())
+}
+
+fn create_start_sh_file(project_name: &str) -> std::io::Result<()> {
+    let mut start_sh = fs::File::create(format!("{}/.docker/start.sh", project_name))?;
+    start_sh
+        .write_all(
+            "#!/bin/bash
+    
+npm install
+
+tail -f /dev/null
+
+    "
+            .as_bytes(),
+        )
+        .unwrap();
+    Ok(())
+}
+
+fn create_dot_docker_folder(project_name: &str) -> std::io::Result<()> {
+    fs::create_dir(format!("{}/.docker", project_name))?;
+    fs::create_dir(format!("{}/.docker/zsh", project_name))?;
+    fs::create_dir(format!("{}/.docker/zsh/history", project_name))?;
+    fs::create_dir(format!("{}/.docker/zsh/powerlevel10k", project_name))?;
+    Ok(())
+}
+
+fn create_docker_compose_yml(project_name: &str) -> std::io::Result<()> {
+    let mut docker_compose = fs::File::create(format!(
+        "{}/.devcontainer/docker-compose.yaml",
+        project_name
+    ))?;
+    docker_compose
+        .write_all("version: '3'
+services:
+    # Update this to the name of the service you want to work with in your docker-compose.yml file
+    app:
+    # If you want add a non-root user to your Dockerfile, you can use the 'remoteUser'
+    # property in devcontainer.json to cause VS Code its sub-processes (terminals, tasks, 
+    # debugging) to execute as the user. Uncomment the next line if you want the entire 
+    # container to run as this user instead. Note that, on Linux, you may need to 
+    # ensure the UID and GID of the container user you create matches your local user. 
+    # See https://aka.ms/vscode-remote/containers/non-root for details.
+    #
+    # user: vscode
+
+    # Uncomment if you want to override the service's Dockerfile to one in the .devcontainer 
+    # folder. Note that the path of the Dockerfile and context is relative to the *primary* 
+    # docker-compose.yml file (the first in the devcontainer.json 'dockerComposeFile'
+    # array). The sample below assumes your primary file is in the root of your project.
+    #
+    # build:
+    #   context: .
+    #   dockerfile: .devcontainer/Dockerfile
+
+    volumes:
+        # Update this to wherever you want VS Code to mount the folder of your project
+        - .:/home/node/app:cached
+
+        # Uncomment the next line to use Docker from inside the container. See https://aka.ms/vscode-remote/samples/docker-from-docker-compose for details.
+        # - /var/run/docker.sock:/var/run/docker.sock 
+
+    # Uncomment the next four lines if you will use a ptrace-based debugger like C++, Go, and Rust.
+    # cap_add:
+    #   - SYS_PTRACE
+    # security_opt:
+    #   - seccomp:unconfined
+         ".as_bytes()).unwrap();
+    Ok(())
+}
+
+fn create_devcontainer_json_file(project_name: &str) -> std::io::Result<()> {
+    let mut devcontainer =
+        fs::File::create(format!("{}/.devcontainer/devcontainer.json", project_name))?;
+    devcontainer.write_all("{
+        'name': 'TypeScript App',
+        'dockerComposeFile': ['../docker-compose.yaml', '../docker-compose.override.yaml', 'docker-compose.yml'],
+        'service': 'app',
+        'workspaceFolder': '/home/node/app',
+        'settings': {
+          'terminal.integrated.defaultProfile.linux': 'zsh'
+        },
+        'extensions': ['firsttris.vscode-jest-runner']
+      }".replace("'", "\"").as_bytes()).unwrap();
+    Ok(())
+}
+
+fn create_dot_dockercontainer_folder(project_name: &str) -> std::io::Result<()> {
+    fs::create_dir(format!("{}/.devcontainer", project_name))?;
+    Ok(())
+}
+
+fn create_docker_compose_override_file(project_name: &str) -> std::io::Result<()> {
+    let mut docker_compose_override =
+        fs::File::create(format!("{}/docker-compose.override.yml", project_name))?;
+    docker_compose_override
+        .write_all(
+            "version: '3'
+
+services:
+  app:
+    volumes:
+      - ./.docker/zsh/powerlevel10k/.p10k.zsh:/home/node/.p10k.zsh:delegated
+      - ./.docker/zsh/history:/home/node/zsh:delegated"
+                .as_bytes(),
+        )
+        .unwrap();
+    Ok(())
+}
+
+fn create_docker_compose_file(project_name: &str) -> std::io::Result<()> {
+    let mut docker_compose = fs::File::create(format!("{}/docker-compose.yml", project_name))?;
+    docker_compose
+        .write_all(
+            "version: '3'
+
+services:
+    app:
+    build: .
+    container_name: PROJECT_NAME
+    command: .docker/start.sh
+    volumes:
+        - .:/home/node/app
+          "
+            .replace("PROJECT_NAME", project_name)
+            .as_bytes(),
+        )
+        .unwrap();
+
+    Ok(())
+}
+
+fn create_docker_file(project_name: &str) -> std::io::Result<()> {
+    let mut docker = fs::File::create(format!("{}/Dockerfile", project_name))?;
+    docker.write_all("FROM node:16.19.1-slim
+
+RUN mkdir -p /usr/share/man/man1 && \
+    echo 'deb http://ftp.debian.org/debian stretch-backports main' | tee /etc/apt/sources.list.d/stretch-backports.list && \
+    apt update && apt install -y \
+    git \
+    ca-certificates \
+    openjdk-11-jre \
+    zsh \
+    curl \
+    wget \
+    fonts-powerline \
+    procps
+
+RUN npm install -g @nestjs/cli@8.2.5 npm@8.5.5
+
+ENV JAVA_HOME=\"/usr/lib/jvm/java-11-openjdk-amd64\"
+
+USER node
+
+WORKDIR /home/node/app
+
+RUN sh -c \"$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.1.2/zsh-in-docker.sh)\" -- \
+    -t https://github.com/romkatv/powerlevel10k \
+    -p git \
+    -p git-flow \
+    -p https://github.com/zdharma-continuum/fast-syntax-highlighting \
+    -p https://github.com/zsh-users/zsh-autosuggestions \
+    -p https://github.com/zsh-users/zsh-completions \
+    -a 'export TERM=xterm-256color'
+
+RUN echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> ~/.zshrc && \
+    echo 'HISTFILE=/home/node/zsh/.zsh_history' >> ~/.zshrc 
+
+CMD [ \"tail\", \"-f\" , \"/dev/null\" ]
+    ".as_bytes()).unwrap();
+    Ok(())
 }
 
 fn create_prettier_file(project_name: &str) -> std::io::Result<()> {
@@ -222,5 +409,14 @@ fn create_project_founder(project_name: &str) -> std::io::Result<()> {
     if !fs::metadata(project_name).is_ok() {
         fs::create_dir(project_name)?;
     }
+    Ok(())
+}
+
+fn create_p10k_file(project_name: &str) -> std::io::Result<()> {
+    let mut p10k = fs::File::create(format!(
+        "{}/.docker/zsh/powerlevel10k/.p10k.zsh",
+        project_name
+    ))?;
+    p10k.write_all("".as_bytes()).unwrap();
     Ok(())
 }
